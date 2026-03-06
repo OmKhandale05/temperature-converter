62076e3b80eeca7e39fb7a26afa8832ca66c97c4
OmPrakash05 <omprakashkhandale5@gmail.com>
2026-03-06 20:00:33 +0530

    create component currency converter and structure code files properly

--- src/App.jsx
+++ src/App.jsx
@@ -1,55 +1,20 @@
-import { useState } from 'react';
 import './App.css'
+import CurrencyConverter from './components/CurrencyConverter';
 import TemperatureConverter from './components/TemperatureConverter';
-import useCurrency from './hooks/useCurrency';
-import useTemperature from './hooks/useTemperature';
+
 
 function App() {
-
-  
-  const {usd, inr, rate, handleUsd, handleInr} = useCurrency();
-
-  
-
 
   return (
 
     <div className='container'>
-      
+
       <TemperatureConverter/>
 
-      <div>
-        <h1>Currency Converter</h1>
-        <p>1 USD = {rate} INR</p>
-        <div className='input-field'>
-          <label>USD</label>
-
-          <input type="number"
-          value={usd}
-          onChange = {handleUsd}
-        
-          />
-          <span> $ </span>
-        </div>
-        <div className='input-field'>
-          <label >INR</label>
-          <input type="number"
-          value={inr}
-          onChange = {handleInr}
-          
-          />
-          <span> ₹ </span>
-
-        </div>
-
-      </div>
-    
+      <CurrencyConverter/>
 
     </div>
-    
-
-    
-    
+  
   );
 }
 
---
+++ src/components/CurrencyConverter.jsx
@@ -0,0 +1,35 @@
+import useCurrency from "../hooks/useCurrency";
+const CurrencyConverter = () => {
+
+    const {usd, inr, rate, handleUsd, handleInr} = useCurrency();
+
+    return(
+        <div>
+            <h1>Currency Converter</h1>
+            <p>1 USD = {rate} INR</p>
+            <div className='input-field'>
+            <label>USD</label>
+
+            <input type="number"
+            value={usd}
+            onChange = {handleUsd}
+            
+            />
+            <span> $ </span>
+            </div>
+            <div className='input-field'>
+            <label >INR</label>
+            <input type="number"
+            value={inr}
+            onChange = {handleInr}
+            
+            />
+            <span> ₹ </span>
+
+            </div>
+
+        </div>
+    
+    )
+}
+export default CurrencyConverter;
