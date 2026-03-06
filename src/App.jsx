d5df607425cb809f0aa65b7f9c816f3fffb12734
OmPrakash05 <omprakashkhandale5@gmail.com>
2026-03-06 17:57:44 +0530

    add currency converter

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,74 @@
 import { useState } from 'react';
 import './App.css'
+import useCurrency from './hooks/useCurrency';
 import useTemperature from './hooks/useTemperature';
 
 function App() {
 
   const {celsius, fahrenheit, handleCelcius, handleFahrenheit} = useTemperature();
+  const {usd, inr, handleUsd, handleInr} = useCurrency();
 
   
 
 
   return (
+
     <div className='container'>
-    <h1>Temperature Converter</h1>
+      <div>
+        <h1>Temperature Converter</h1>
 
-    <div className='input-field'>
-      <label>Celsius</label>
+        <div className='input-field'>
+          <label>Celsius</label>
 
-      <input type="number"
-      value={celsius}
-      onChange = {handleCelcius}
-      />
-      <span> °C </span>
+          <input type="number"
+          value={celsius}
+          onChange = {handleCelcius}
+          />
+          <span> °C </span>
+        </div>
+        <div className='input-field'>
+          <label>fahrenheit</label>
+
+          <input type="number"
+          value={fahrenheit}
+          onChange = {handleFahrenheit}
+          
+          />
+          <span> °F </span>
+        </div>
+
+      </div>
+
+      <div>
+        <h1>Currency Converter</h1>
+        <div className='input-field'>
+          <label>USD</label>
+
+          <input type="number"
+          value={usd}
+          onChange = {handleUsd}
+        
+          />
+          <span> $ </span>
+        </div>
+        <div className='input-field'>
+          <label >INR</label>
+          <input type="number"
+          value={inr}
+          onChange = {handleInr}
+          
+          />
+          <span> ₹ </span>
+
+        </div>
+
+      </div>
+    
+
     </div>
-    <div className='input-field'>
-      <label>fahrenheit</label>
+    
 
-      <input type="number"
-      value={fahrenheit}
-      onChange = {handleFahrenheit}
-      
-      />
-      <span> °F </span>
-    </div>
-
-    </div>
+    
     
   );
 }
---
+++ src/hooks/useCurrency.js
@@ -0,0 +1,24 @@
+import { useState } from "react";
+
+const useCurrency = () =>{
+
+    const [usd, setUSD] = useState("");
+    const [inr, setINR] = useState("");
+
+    const rate = 91.7;
+
+    const handleUsd = (e) => {
+        const value = e.target.value;
+        setUSD(value);
+        setINR(value * rate);
+    }
+
+    const handleInr = (e) => {
+        const value = e.target.value;
+        setINR(value);
+        setUSD(value / rate);
+    }
+    return{ usd, inr, handleUsd, handleInr}
+}
+
+export default useCurrency;
